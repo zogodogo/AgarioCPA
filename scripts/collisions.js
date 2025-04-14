f0d9d80c5ea813e744b03313e221577334684b14
amonbofis <gabriel.zamy15@gmail.com>
2025-04-14 17:10:02 +0200

    all collision set, split implemented

--- scripts/collisions.js
+++ scripts/collisions.js
@@ -14,6 +14,7 @@ var context=main_window.getContext('2d');
 var worldWidth = 2000;
 var worldHeight = 2000;
 
+const gravityStrength = 0.1;
 
 var width=main_window.width;
 var height=main_window.height;
@@ -21,8 +22,8 @@ var friction=0.98;
 
 
 var red = new player(0x51E77E,
-                    new avatar(3*worldWidth/4,worldHeight/2,height/6,"#FF0000","#FF2400"),
-                    new keys(0x25,0x27,0x26,0x28)
+                    new avatar(3*worldWidth/4,worldHeight/2,height/8,"#FF0000","#FF2400"),
+                    new keys(0x25,0x27,0x26,0x28, 0x20) // left, right, up, down, space
                     );
   
 var p1 = new food(
@@ -48,15 +49,12 @@ var bushes = new Array(bush1);
 var camera = {
   x: 0,
   y: 0,
-  follow: red.avatar, // main player
+  follow: red.avatars[0], // main player
   update: function() {
       // Center the camera on the main player
       this.x = this.follow.x - width / 2;
       this.y = this.follow.y - height / 2;
 
-      // Clamp camera within world boundaries
-      this.x = Math.max(0, Math.min(this.x, worldWidth - width));
-      this.y = Math.max(0, Math.min(this.y, worldHeight - height));
   }
 };
 
@@ -71,6 +69,14 @@ function drawWorldBorder() {
       worldWidth,
       worldHeight
   );
+}
+
+function getRandomPositionInCircle(centerX, centerY, radius) {
+  const angle = Math.random() * 2 * Math.PI;
+  const r = Math.sqrt(Math.random()) * radius; // sqrt for uniform distribution
+  const x = centerX + r * Math.cos(angle);
+  const y = centerY + r * Math.sin(angle);
+  return { x, y };
 }
 
 
@@ -121,6 +127,8 @@ function avatar(x,y,r,c,bc){
   this.radius=r;
   this.color=c;
   this.bordercolor=bc;
+  this.vx = 0; // new!
+  this.vy = 0; // new!
 }
 
 function buisson(x,y,r){
@@ -131,79 +139,158 @@ function buisson(x,y,r){
 
 }
 
-function keys(l,r,u,d){
+function keys(l,r,u,d,s){
   this.left={code:l,hold:false};
   this.right={code:r,hold:false};
   this.up={code:u,hold:false};
   this.down={code:d,hold:false};
+  this.space={code:s,hold:false};
 }
 
-function player(id, avatar, keys){
+function player(id, avatar1, keys){
   this.id=id;
-  this.avatar=avatar;
+  this.avatars=[avatar1];
   this.keys=keys;
-  this.vx=0;
-  this.vy=0;
+
+  //this part will be for a kind of gravity center that has to bring closer all the avatars
+  
+
+  this.centerX=this.avatars[0].x;
+  this.centerY=this.avatars[0].y; 
+
+
+  this.applyInternalGravity=applyInternalGravity;
+  function applyInternalGravity(){
+      let totx = 0;
+      let toty = 0;
+      for(let i = 0; i < this.avatars.length; i++){
+        totx += this.avatars[i].x;
+        toty += this.avatars[i].y;
+      }
+      this.centerX = totx / this.avatars.length;
+      this.centerY = toty / this.avatars.length;
+
+  }
 
   this.updateFriction=updateFriction;
-  function updateFriction(){ this.vx*=friction; this.vy*=friction; }
+  function updateFriction(){ 
+    for(let i = 0; i < this.avatars.length; i++){
+      this.avatars[i].vx *= friction;
+      this.avatars[i].vy *= friction;
+    }
+    //this.vx*=friction; this.vy*=friction; 
+  }
 
   this.updateCommands=updateCommands;
   function updateCommands(){ 
-    if(this.keys.left.hold){this.vx--;}
-    if(this.keys.right.hold){this.vx++;}
-    if(this.keys.up.hold){this.vy--;}
-    if(this.keys.down.hold){this.vy++;}
+    for(let i = 0; i < this.avatars.length; i++){
+
+      if(this.keys.left.hold){this.avatars[i].vx -= 0.5;}//to handle the speed of the player (1-this.avatar.radius)
+      if(this.keys.right.hold){this.avatars[i].vx+= 0.5;}
+      if(this.keys.up.hold){this.avatars[i].vy-= 0.5;}
+      if(this.keys.down.hold){this.avatars[i].vy+= 0.5;}
+      if(this.keys.space.hold){console.log("space split"); this.split();}
+    }
   }
 
   this.updateCollisionBorder=updateCollisionBorder;
-  function updateCollisionBorder(){ 
-    if (collisionLeftBorder(avatar)){ this.vx*=-0.5; this.avatar.x=this.avatar.radius; return true; }
-    if (collisionRightBorder(avatar)){ this.vx*=-0.5; this.avatar.x=worldWidth-this.avatar.radius; return true; }
-    if (collisionTopBorder(avatar)){ this.vy*=-0.5; this.avatar.y=this.avatar.radius; return true; }
-    if (collisionBottomBorder(avatar)){ this.vy*=-0.5; this.avatar.y=worldHeight-this.avatar.radius; return true; }
+  function updateCollisionBorder(){
+    for(let i = 0; i < this.avatars.length; i++){
+      if(collisionLeftBorder(this.avatars[i])){ this.avatars[i].vx*=-0.5; this.avatars[i].x=this.avatars[i].radius; }
+      if(collisionRightBorder(this.avatars[i])){ this.avatars[i].vx*=-0.5; this.avatars[i].x=worldWidth-this.avatars[i].radius; }
+      if(collisionTopBorder(this.avatars[i])){ this.avatars[i].vy*=-0.5; this.avatars[i].y=this.avatars[i].radius; }
+      if(collisionBottomBorder(this.avatars[i])){ this.avatars[i].vy*=-0.5; this.avatars[i].y=worldHeight-this.avatars[i].radius; }
+    }
     return false;
   }
   
   this.updateCollisionSameMass=updateCollisionSameMass;
   function updateCollisionSameMass(otherPlayer){
-    if(collisionCircles(this.avatar, otherPlayer.avatar )&& this.avatar.radius >= (otherPlayer.avatar.radius +3)){
-      this.avatar.radius += otherPlayer.avatar.radius /10;
-      otherPlayer.avatar.radius = 0;
-      return true;
+    for(let i = 0; i < this.avatars.length; i++) {
+      if(collisionCircles(this.avatars[i], otherPlayer.avatars[0] ) && this.avatars[i].radius >= (otherPlayer.avatars[0].radius +3)){
+        this.avatars[i].radius += otherPlayer.avatars[0].radius /10;
+        otherPlayer.avatars[0].radius = 0;
+        console.log("collision between "+this.id+" and "+otherPlayer.id);
+        return true;
+      }
     }
     return false;
   }
 
   this.updateCollisionFood=updateCollisionFood;
   function updateCollisionFood(someFood){
-    if(collisionCircles(this.avatar, someFood.avatar)){
-      this.avatar.radius += someFood.avatar.radius /5;
-      someFood.avatar.radius = 0;
-      return true;
+    for (let i = 0; i < this.avatars.length; i++) {
+      if(collisionCircles(this.avatars[i], someFood.avatar)){
+        this.avatars[i].radius += someFood.avatar.radius /5;
+        someFood.avatar.radius = 0;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  this.updateBushCollision=updateBushCollision;
+  function updateBushCollision(aBush){
+    for (let i = 0; i < this.avatars.length; i++) {
+      if(collisionCircles(this.avatars[i], aBush.buisson)&& this.avatars[i].radius >= (aBush.buisson.radius +3)){
+        this.split();
+        // this.avatars[i].radius += aBush.buisson.radius /5;
+        // aBush.avatar.radius = 0;
+        return true;
+      }
     }
     return false;
   }
 
   this.updatePosition=updatePosition;
-  function updatePosition(){ this.avatar.x+=this.vx; this.avatar.y+=this.vy; }
+  function updatePosition(){ 
+    for (let i = 0; i < this.avatars.length; i++) {
+        this.avatars[i].x += this.avatars[i].vx;
+        this.avatars[i].y += this.avatars[i].vy;
+    }
+  }
  
   this.draw=draw;
   function draw() {
-      context.beginPath();
-      var g = context.createRadialGradient(
-          this.avatar.x - camera.x, this.avatar.y - camera.y,
-          this.avatar.radius * 0.98,
-          this.avatar.x - camera.x, this.avatar.y - camera.y,
-          this.avatar.radius
-      );
-      g.addColorStop(0, this.avatar.color);
-      g.addColorStop(1, this.avatar.bordercolor);
-      context.fillStyle = g;
-      context.arc(this.avatar.x - camera.x, this.avatar.y - camera.y,
-                  this.avatar.radius, 0, Math.PI * 2, true);
-      context.fill();
-      context.closePath();
+    for (let i = 0; i < this.avatars.length; i++) {
+        context.beginPath();
+        var g = context.createRadialGradient(
+            this.avatars[i].x - camera.x, this.avatars[i].y - camera.y,
+            this.avatars[i].radius * 0.98,
+            this.avatars[i].x - camera.x, this.avatars[i].y - camera.y,
+            this.avatars[i].radius
+        );
+        g.addColorStop(0, this.avatars[i].color);
+        g.addColorStop(1, this.avatars[i].bordercolor);
+        context.fillStyle = g;
+        context.arc(this.avatars[i].x - camera.x, this.avatars[i].y - camera.y,
+                    this.avatars[i].radius, 0, Math.PI * 2, true);
+        context.fill();
+        context.closePath();
+    }
+  }
+
+  this.split = split;
+  function split() {
+
+    if (this.avatars.length >= 16) return; // limit split to prevent abuse
+    for(let i = 0; i < this.avatars.length; i++) {
+      if (this.avatars[i].radius > 100){ // if not too small to split
+        let newRadius = this.avatars[i].radius / Math.sqrt(2); // keeps area same (πr²)
+        this.avatars[i].radius = newRadius;
+        let angle = Math.atan2(this.avatars[i].vy, this.avatars[i].vx); // movement angle
+        let distance = newRadius * 2;
+        let speed = 8;
+        let newX = this.avatars[i].x + Math.cos(angle) * distance;
+        let newY = this.avatars[i].y + Math.sin(angle) * distance;
+
+        let newAvatar = new avatar(newX, newY, newRadius, this.avatars[i].color, this.avatars[i].bordercolor);
+        
+        newAvatar.vx = Math.cos(angle) * speed;
+        newAvatar.vy = Math.sin(angle) * speed;
+        this.avatars.push(newAvatar);
+      } 
+    }
   }
 }
 
@@ -271,7 +358,6 @@ for (let i = 0; i < 9; i++) {
   players.push(staticPlayer);
 }
 
-
 function on_enter_frame(){
 
     camera.update();
@@ -280,6 +366,7 @@ function on_enter_frame(){
     for (var i = 0; i < players.length; i++) {
         players[i].updateFriction();
         players[i].updateCommands();
+        players[i].applyInternalGravity();
         players[i].updateCollisionBorder();
     }
   
@@ -287,12 +374,14 @@ function on_enter_frame(){
     // has to be done in different loop otherwise not every players are checked
     var tmpPlayers = []
     var tmpFood = []
+    var tmpBush = []
     for(var j=0;j<players.length;j++){
         var collisionCheck=false;
         var foodCollisionCheck=false;
+        var bushCollisionCheck=false;
         for (var i=0;i<players.length;i++) {
-            if(i!=j){                
-                collisionCheck=players[i].updateCollisionSameMass(players[j]);          
+            if(i!=j){      
+                collisionCheck|=players[i].updateCollisionSameMass(players[j]);          
             }
         }
         
@@ -302,10 +391,13 @@ function on_enter_frame(){
             tmpPlayers.push(j);
         }
         for (var k=0;k<ressources.length;k++){
-            foodCollisionCheck = players[j].updateCollisionFood(ressources[k]);
+            foodCollisionCheck |= players[j].updateCollisionFood(ressources[k]);
             if(foodCollisionCheck){
                 tmpFood.push(k);
             }
+        }
+        for (var l=0;l<bushes.length;l++){
+          bushCollisionCheck |= players[j].updateBushCollision(bushes[l]);
         }
     }
 
@@ -318,6 +410,11 @@ function on_enter_frame(){
     tmpFood.sort(function(a, b) { return b - a; });  // Tri décroissant
     for( var k=0; k<tmpFood.length; k++){
         ressources.splice(tmpFood[k],1);
+    }
+
+    tmpBush.sort(function(a, b) { return b - a; });  // Tri décroissant
+    for( var k=0; k<tmpBush.length; k++){
+        bushes.splice(tmpBush[k],1);
     }
     
     
